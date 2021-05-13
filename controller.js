a72f27daadf7853173ec9940ae28cb7a0584a620
Ivan MPR <ivsanmas@gmail.com>
2021-05-13 17:34:33 +0200

    Initial JS added, modules implementation

---
+++ controller.js
@@ -0,0 +1,71 @@
+// ******* Selections ******** //
+//prettier-ignore
+import { playOnStartTone, trueAswerTone, falseAswerTone, playOnLevelEndTone } from './modules/audio.js';
+
+////////////////////////////////////////
+//*** Buttons ***//
+const startBtn = document.querySelector('.start');
+const restartLevelBtn = document.querySelector('.restart');
+const quitBtn = document.querySelector('.quit');
+///////////////////////////////////////////////////////
+const languageChoice = document.getElementById('language');
+const continentChoice = document.getElementById('continents');
+const flag = document.querySelector('.flag');
+////////////////////////////////////////////////////////
+const answersGrid = document.querySelector('.answers-grid');
+const choices = document.querySelectorAll('.choices');
+const hits = document.getElementById('hits');
+const misses = document.getElementById('misses');
+//////////////////////////////////////////////////////////
+const gameInfo = document.getElementById('game-info-text');
+const time = document.querySelector('.time');
+const additionalInfoContainer = document.querySelector(
+  '.additional-info-container'
+);
+const totalFlagsInLevel = document.querySelector('.total-flags-in-level');
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+function visibleConstants() {
+  restartLevelBtn.classList.remove('hidden');
+  quitBtn.classList.remove('hidden');
+  totalFlagsInLevel.classList.remove('hidden');
+  time.classList.remove('hidden');
+  restartLevelBtn.classList.add('visible');
+  quitBtn.classList.add('visible');
+  totalFlagsInLevel.classList.add('visible');
+  time.classList.add('visible');
+}
+
+function clearFields() {
+  gameInfo.textContent = '';
+  choices.forEach(choice => {
+    choice.textContent = '';
+    choice.classList.remove('true', 'false');
+  });
+}
+function resetFlag() {
+  const path = 'url(images/empty.jpg)';
+  flag.style.backgroundImage = path;
+}
+function hideStartBtn() {
+  startBtn.classList.add('hidden');
+}
+
+function init() {
+  clearFields();
+  resetFlag();
+  hideStartBtn();
+  playOnStartTone();
+}
+
+/////////////////////////////////////////////////////////////////////
+// Test selection and audio helper functions
+// function checkSelection(element) {
+//   // console.log(element);
+//   // element.forEach(div => div.classList.add('hidden'));
+//   element.classList.add('hidden');
+// }
+// checkSelection(additionalInfoContainer);
+
+startBtn.addEventListener('click', init);
+startBtn.addEventListener('click', visibleConstants);
---
+++ modules/audio.js
@@ -0,0 +1,21 @@
+/*** Audio elements***/
+export const playOnStartTone = function () {
+  const tone = new Audio(
+    '../sounds/positive/330052__paulmorek__beep-05-positive.wav'
+  );
+  return tone.play();
+};
+export const trueAswerTone = function () {
+  const tone = new Audio('../sounds/positive/528862__eponn__beep-4.wav');
+  return tone.play();
+};
+export const falseAswerTone = function () {
+  const tone = new Audio(
+    '../sounds/negative/253886__themusicalnomad__negative-beeps.wav'
+  );
+  return tone.play();
+};
+export const playOnLevelEndTone = function () {
+  const tone = new Audio('../sounds/positive/320775__rhodesmas__win-02.wav');
+  return tone.play();
+};
