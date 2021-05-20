e2a0fccc48a29893f2a04aaf4f3260166dce3042
Ivan MPR <ivsanmas@gmail.com>
2021-05-20 23:08:46 +0200

    cheerMessage, paintGreenBackground, paintRedBackground added

--- modules/view.js
+++ modules/view.js
@@ -1,3 +1,5 @@
+import { gameInfo } from '../controller.js';
+
 export function greenThumbFlash() {
   hits.classList.add('green-b-b');
   setTimeout(() => {
@@ -8,5 +10,26 @@ export function redThumbFlash() {
   misses.classList.add('red-b-b');
   setTimeout(() => {
     misses.classList.remove('red-b-b');
+  }, 400);
+}
+
+export function cheerMessage() {
+  //prettier-ignore
+  const messages = ['Good Job', 'Nice Work', 'Way to Go', 'Keep Going', 'Awesome', 'Fantastic Work']
+  const randomNumber = Math.floor(Math.random() * messages.length);
+  return (gameInfo.textContent = `Correct Answer ! ${messages[randomNumber]} !`);
+}
+
+export function paintGreenBackground(element) {
+  element.style.backgroundColor = 'var(--green-color)';
+  setTimeout(() => {
+    element.style.backgroundColor = 'var(--fields-background-color)';
+  }, 400);
+}
+
+export function paintRedBackground(element) {
+  element.style.backgroundColor = 'var(--red-color)';
+  setTimeout(() => {
+    element.style.backgroundColor = 'var(--fields-background-color)';
   }, 400);
 }
